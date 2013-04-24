1ad1dbbbbb0e54918ba4abcfd7727c8c44c1ceab
Hadi Wirontono <hadi.wirontono@gmail.com>
2013-04-24 19:36:36 -0400

    adding 5 layers

---
+++ Hadoz/HadozDataModel/BlogPostDataModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HadozDataModel
+{
+    public class BlogPostDataModel
+    {
+        public int PostID { get; set; }
+        public string Title { get; set; }
+        public StringBuilder Body { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public string Author { get; set; }
+        public List<string> Tags { get; set; }
+
+    }
+}
---
+++ Hadoz/HadozViewModel/BlogPostViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using HadozDataModel;
+
+namespace HadozViewModel
+{
+    public class BlogPostViewModel
+    {
+        public BlogPostDataModel BlogPost { get; set; }
+    }
+}
